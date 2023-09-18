9eb2ee9211953cd4d139ac21881b9699f625e80e
Sohel Rana <sohelcuetcse11@gmail.com>
2023-09-18 15:05:23 +0600

    feat: adding game skeloton

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -6,7 +6,7 @@ import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 
 const GameCard = ({ game }: GameProps) => {
     return (
-        <Card borderRadius={10} overflow="hidden">
+        <Card width="300px" borderRadius={10} overflow="hidden">
             <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
