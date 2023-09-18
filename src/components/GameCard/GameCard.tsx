9c62a9bbfa43cd2f3e6de7af9b78bb3098e4a2d8
Sohel Rana <sohelcuetcse11@gmail.com>
2023-09-18 14:52:00 +0600

    feat: get optimized imageas

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,12 +1,13 @@
+import getCroppedImageUrl from '../../services/image-url';
 import CriticScore from '../CriticScore';
 import PlatformIconList from '../PlatformIconList';
 import { GameProps } from './types';
-import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
+import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 
 const GameCard = ({ game }: GameProps) => {
     return (
         <Card borderRadius={10} overflow="hidden">
-            <Image src={game.background_image} />
+            <Image src={getCroppedImageUrl(game.background_image)} />
             <CardBody>
                 <Heading fontSize="2xl">{game.name}</Heading>
                 <HStack justifyContent="space-between">
